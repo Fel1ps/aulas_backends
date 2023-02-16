beeb99517c8e32a4add101cf82e56aa735822c0c
Felipe da Silva Santos <13490993683@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-16 10:39:51 -0300

    first commit

---
+++ aula1/exemplo1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        for($c = 1; $c <=50; $c = $c +1) {
+    ?>   
+<img src="https://lavrapalavra.com/wp-content/uploads/2020/10/batman.jpg" alt="Batman" width="10%">
+    
+<?php } ?>
+</body>
+</html>
