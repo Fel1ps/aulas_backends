7e4f654064ac0c139e8758a318a3d6e5a188a613
Felipe da Silva Santos <13490993683@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-20 08:41:43 -0300

    aula GET

---
+++ aula12/get/exemplo_get.php
@@ -0,0 +1,10 @@
+
+
+<?php
+   $resultado = $_GET['numero1'] + $_GET['numero2'];
+   echo "O resultado da soma é $resultado";
+
+//Por meio da url, criamos as variaveis 'numero1' e 'numero2'.
+//no php criamos uma variavel chamada 'resultado' e fizemos a conta 'numero1' + 'numero2' = 'resultado'
+//e requisitamos o resultado da soma que é o valor do 'resultado'.
+?>
