8d7c2e57c962c0a718a8fadd332ad5c26c057bd9
Felipe da Silva Santos <13490993683@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-08 08:41:40 -0300

    aula 17 correção insset

--- aula18/produto/excluir.php
+++ aula18/produto/excluir.php
@@ -2,26 +2,27 @@
 
    //inclui o codigo da 'conexao.php' para essa linha
    require_once "../conexao.php";
+  
+  //verifica se foi enviado o param id pela 
+  if(isset($_GET['id'])){
 
-   if(isset($_POST["nome"]) && isset($_POST["descricao"]) && isset($_POST["preco"]))
-   {
-   $nome = $_POST["nome"];
-   $descricao = $_POST["descricao"];
-   $preco = $_POST["preco"];
-   $foto ="semfoto.png";
+   //pega o valor do id que foi enviado pela url
+   $id = $_GET['id'];
   
    //String com o comando SQL para ser executado no DB
-  $sql = "INSERT INTO `produto` (`nome`, `preco`, `descricao`, `foto`) VALUES (?, ?, ?, ?);";
+  $sql = "DELETE FROM `produto` WHERE  `idproduto`= ? ;";
   echo $sql;
 
 //Prepara o SQL para ser executado no banco de dados
 $comando = $conexao->prepare($sql);
 
 //adiciona os valores nos parâmetros
-$comando->bind_param("ssds", $nome, $descricao, $preco, $foto);
+$comando->bind_param("i", $id);
 
 //executa o SQL - Comando no Banco de Dados
 $comando->execute();
-   }
+
+}
+
 //abre o arquivo form.php
 header("Location: form.php");
